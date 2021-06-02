f8c0ad7896e46050d868aed7e315b4cbd2e4bc71
JoseIgnacioTapia <josignaciot@gmail.com>
2021-06-02 17:30:11 -0300

    Formulario Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import imagen from './cryptomonedas.png';
+import Formulario from './components/Formulario';
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -45,6 +46,8 @@ function App() {
       </div>
       <div>
         <Heading>Cotiza Criptomonedas al instante</Heading>
+        
+        <Formulario />
       </div>
     </Contenedor>
   );
